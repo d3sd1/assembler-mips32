951288c6f794ca7838751d7a30ba854fcc0f35e0
Miguel Hernández Cassel <61021414+Miguelherca00@users.noreply.github.com>
2020-11-07 23:24:22 +0100

    Add files via upload

---
+++ Ejercicio1 v2.s
@@ -0,0 +1,69 @@
+
+##EJERCICIO 1
+.text
+.globl main
+
+#init(int A[][], int M, int N)
+# A -> Matriz MxN.
+# M -> Número de filas.
+# N -> Número de columnas.
+#Inicializa la matriz a 0. Si M <= 0 o N <= 0, retorna -1 como error, en cualquier otro caso devuelve 0.
+
+  init: addu $sp, $sp, -12
+	  	sw $a0, 12($sp)
+      	sw $a1, 8($sp)
+      	sw $a2, 4($sp)
+        li $a0, 0
+      	li $a1, 4	# M <- $a1 (M = 4)
+      	li $a2, 7	# N <- $a2 (N = 7)
+        mul $a0, $a1, $a2 # A = MxN
+        #Si los valores de M o N son menores que 0, se dirige a fin_error
+        blez $a1, fin_error 
+        blez $a2, fin_error
+        # for(i=0; i<=M, i++)
+      	li $t0, 0	# i = $t0 (iniciado a 0)
+bucle1: bge $t0, $a1, fin1	#Cuando i >= M, ir a fin_1
+		# for(j=0, j<=N, j++)
+        li $t1, 0	# j = $t1 (iniciado a 0)
+bucle2: bge $t1, $a2, fin2	#Cuando j >= N, ir a fin_2 		
+			# Dirección del elemento (i,j)
+			mul  $t2 $t0 $a2 # i*N <- $t3
+            add  $t2 $t2 $t1 # + j
+            mul  $t2 $t3 4   # * 4
+            add  $t2 $a0 $t2 # A + (i*N + j)*4
+            # A[i][j] = 0
+            sw $zero, ($t2) # memoria[$t2] = 0			
+ 		addi $t1 $t1, 1	#j++
+ 		b bucle2
+  fin2:
+ 		addi $t0 $t0, 1	#i++
+ 		b bucle1
+  fin1:	
+  		li $v0 0 #Resultado
+        lw $a2, 4($sp)
+    	lw $a1, 8($sp)
+   		lw $a0, 12($sp)
+    	addu $sp, $sp, 12
+ 		jr $ra	#Return
+        
+ fin_error: li $v0 -1	#Resultado en caso de error
+ 			lw $a2, 4($sp)
+    		lw $a1, 8($sp)
+   			lw $a0, 12($sp)
+    		addu $sp, $sp, 12
+ 			jr $ra	#Return
+
+#add(int A[][], int M, int N, int i, int j, int k, int l)
+# A -> Matriz MxN.
+# M -> Número de filas.
+# N -> Número de columnas.
+#Suma los valores de la matriz comprendidos entre los elementos (i,j) y (k,l)
+# i -> entero correspondiente a la fila del primer elemento de la matriz
+# j -> entero correspondiente a la columna del primer elemento de la matriz
+# k -> entero correspondiente a la fila del último elemento de la matriz 
+# l ->  entero correspondiente a la columna del último elemento de la matriz
+
+
+   main:
+    	jal init
+        
